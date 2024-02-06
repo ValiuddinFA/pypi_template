f3b3f70d3ae1217479401642ac6242be5ec049e3
Valiuddin Qureshi <valiuddin.qureshi@forgeahead.io>
2024-02-06 19:37:48 +0530

    first commit

---
+++ init_setup.sh
@@ -0,0 +1,8 @@
+echo [$(date)]: "START"
+echo [$(date)]: "Creating Conda Env with Python 3.10"
+conda create --prefix ./env python=3.10 -y
+echo [$(date)]: "Activating env"
+source activate env
+echo [$(date)]: "Installing dev requirements"
+pip install -r requirements_dev.txt
+echo [$(date)]: "END"
---
+++ template.py
@@ -0,0 +1,40 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s: %(levelname)s]: %(message)s"
+)
+
+while True:
+    project_name = input("Enter the project name: ")
+    if project_name != ' ':
+        break
+
+logging.info(f"Creating project by name: {(project_name)}")
+
+#list of files
+list_of_files = [
+    ".github/.workflows/.gitkeep",
+    f"tests/__init__.py",
+    f"tests/unit/__init__.py",
+    f"tests/integration/__init__.py",
+    f"src/{project_name}/__init__.py",
+    "pyproject.toml",
+    "setup.cfg",
+    "tox.ini"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(Path(filepath))
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating a directory at: {filedir} for {filename}")
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath, 'w') as f:
+            pass
+            logging.info(f"Creating a new file: {filename} at path: {filepath}")
+    else:
+        logging.info(f"File already exists at: {filepath}")
